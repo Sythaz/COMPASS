5a17a16967fd688cc143a663a04a8db59c13afc4
Muhammad Syafiq Aldiansyah <74223284+Sythaz@users.noreply.github.com>
2025-05-05 18:06:57 +0700

    chore: initialize Laravel project template with quixlab (merge)

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="nk-sidebar">
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label">Dashboard</li>
+            <li>
+                <!-- Jika aktif tambahkan class "active" -->
+                {{-- <a class="has-arrow {{ $activeMenu == 'dashboard' ? 'active' : '' }}" href="javascript:void()" aria-expanded="false"> --}}
+                <a href="javascript:void()" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                </a>
+                <!-- Dropdown
+                    <ul aria-expanded="false">
+                        {{-- Dropdown --}}
+                        <li><a href="./index.html">Home 1</a></li>
+                    </ul>
+                -->
+            </li>
+        </ul>
+    </div>
+</div>
