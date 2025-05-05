b78bd1239970cab0c265d6bcfa31fe6a3e6e8217
Sythaz <syafiqaldiansyah@gmail.com>
2025-05-05 17:31:23 +0700

    chore: initialize Laravel project template with quixlab

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="container-fluid">
+        {{-- <h1>Home</h1> --}}
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="mb-3">Selamat Datang</h4>
+                        <!-- teks di sistem informasi prestasi mahasiswa dan rekomendasi peserta lomba -->
+                        <p class="fw-bolder">Anda berada di halaman utama sistem informasi prestasi mahasiswa dan rekomendasi peserta lomba</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores repellendus molestiae exercitationem voluptatem tempora quo dolore nostrum dolor consequuntur itaque, alias fugit. Architecto rerum animi velit, beatae corrupti quos nam saepe asperiores aliquid quae culpa ea reiciendis ipsam numquam laborum aperiam. Id tempore consequuntur velit vitae corporis, aspernatur praesentium ratione!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('css')
+@endpush
