261ef268c9098adead52dbbc6f430d21f9b7ad3d
keisyansra <keisya.naulia@gmail.com>
2025-05-13 21:12:39 +0700

    fix: Models

--- app/Models/LevelMinatBakatModel.php
+++ app/Models/LevelMinatBakatModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class MinatBakatModel extends Model
+class LevelMinatBakatModel extends Model
 {
     use HasFactory;
 
