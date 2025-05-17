169e197d6ba3349a23042906e10b561add7adaac
Sythaz <syafiqaldiansyah@gmail.com>
2025-05-17 18:12:10 +0700

    refactor: custom form validation to be reusable and fix bugs and revert datatable-basic.min.js to original code, nonactive dashboard-1.js

--- public/js-custom/form-validation.js
+++ public/js-custom/form-validation.js
@@ -1,55 +1,56 @@
-// $.ajaxSetup({
-//     headers: {
-//         "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-//     },
-// });
+// reusable-form.js
 
-// function setupFormValidation(formSelector, rules, onSuccessCallback) {
-//     $(formSelector).validate({
-//         rules: rules,
-//         submitHandler: function (form) {
-//             $.ajax({
-//                 url: form.action,
-//                 type: form.method,
-//                 data: $(form).serialize(),
-//                 success: function (response) {
-//                     if (response.status) {
-//                         Swal.fire({
-//                             icon: "success",
-//                             title: "Berhasil",
-//                             text: response.message,
-//                         }).then(function () {
-//                             if (typeof onSuccessCallback === "function") {
-//                                 onSuccessCallback(response);
-//                             } else if (response.redirect) {
-//                                 window.location = response.redirect;
-//                             }
-//                         });
-//                     } else {
-//                         $(".error-text").text("");
-//                         $.each(response.msgField, function (prefix, val) {
-//                             $("#error-" + prefix).text(val[0]);
-//                         });
-//                         Swal.fire({
-//                             icon: "error",
-//                             title: "Terjadi Kesalahan",
-//                             text: response.message,
-//                         });
-//                     }
-//                 },
-//             });
-//             return false;
-//         },
-//         errorElement: "span",
-//         errorPlacement: function (error, element) {
-//             error.addClass("invalid-feedback");
-//             element.closest(".input-group").append(error);
-//         },
-//         highlight: function (element) {
-//             $(element).addClass("is-invalid");
-//         },
-//         unhighlight: function (element) {
-//             $(element).removeClass("is-invalid");
-//         },
-//     });
-// }
+/**
+ * Inisialisasi validasi form secara reusable
+ * @param {string} formId - ID dari form (misalnya '#form-login')
+ * @param {object} rules - Aturan validasi jQuery Validate
+ * @param {object} messages - Pesan error untuk setiap field
+ * @param {function} successCallback - Fungsi yang dijalankan saat submit berhasil
+ */
+function customFormValidation(formId, rules, messages, successCallback) {
+    $(document).ready(function () {
+        // Setup CSRF token untuk semua AJAX request
+        $.ajaxSetup({
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+        });
+
+        // Validasi form
+        $(formId).validate({
+            rules: rules,
+            messages: messages,
+            submitHandler: function (form) {
+                $.ajax({
+                    url: form.action,
+                    type: form.method,
+                    data: $(form).serialize(),
+                    success: function (response) {
+                        if (typeof successCallback === "function") {
+                            successCallback(response, form);
+                        }
+                    },
+                    error: function (xhr) {
+                        Swal.fire(
+                            "Error",
+                            "Terjadi kesalahan pada server.",
+                            "error"
+                        );
+                    },
+                });
+                return false;
+            },
+            errorElement: "span",
+            errorPlacement: function (error, element) {
+                error.addClass("invalid-feedback");
+                element.closest(".custom-validation").append(error);
+            },
+            highlight: function (element, errorClass, validClass) {
+                $(element).addClass("is-invalid");
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                $(element).removeClass("is-invalid");
+            },
+        });
+    });
+}
