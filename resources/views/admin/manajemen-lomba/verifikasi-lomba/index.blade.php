7a16d62c73e2e8efc9182b30ce09433ec8ebdede
Sythaz <syafiqaldiansyah@gmail.com>
2025-05-26 19:03:59 +0700

    feat: Add LombaVerification feature with controller and routes for Lomba

---
+++ resources/views/admin/manajemen-lomba/verifikasi-lomba/index.blade.php
@@ -0,0 +1,206 @@
+@extends('layouts.template')
+
+@section('title', 'Verifikasi Lomba | COMPASS')
+
+@section('page-title', 'Verifikasi Lomba')
+
+@section('page-description', 'Halaman untuk memverifikasi lomba!')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">                    
+                    <div class="table-responsive">
+                        <table class="w-100 table table-striped table-bordered custom-datatable" id="tabel-verifikasi-lomba">
+                            <thead>
+                                <tr>
+                                    <th style="width: 1px; white-space: nowrap;">No</th>
+                                    <th>Nama Lomba</th>
+                                    <th>Kategori</th>
+                                    <th>Tingkat</th>
+                                    <th>Awal Reg.</th>
+                                    <th>Akhir Reg.</th>
+                                    <th>Status</th>
+                                    <th class="text-center" style="width: 1px; white-space: nowrap;">Aksi</th>
+                                </tr>
+                            </thead>
+                        </table>
+                        {{-- Custom Pagination DataTables --}}
+                        <div class="bootstrap-pagination"></div>
+                    </div>
+                </div>
+                <!-- Modal Bootstrap untuk AJAX (Hanya 1 modal) -->
+                <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="ajaxModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content" id="ajaxModalContent">
+                            <!-- Konten modal akan dimuat via AJAX -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('css')
+    <!-- DataTables CSS -->
+    <link href="{{ asset('theme/plugins/tables/css/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    {{-- Custom Pagination DataTables CSS --}}
+    <link href="{{ asset('css-custom/pagination-datatables.css') }}" rel="stylesheet">
+
+    <!-- CSS Select2 -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2-bootstrap-theme@0.1.0-beta.10/dist/select2-bootstrap.min.css"
+        rel="stylesheet" />
+@endpush
+
+@push('js')
+    <!--  Script DataTables -->
+    <script src="{{ asset('theme/plugins/tables/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('theme/plugins/tables/js/datatable/dataTables.bootstrap4.min.js') }}"></script>
+
+    <!-- JS Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script>
+        var idDataTables = '#tabel-verifikasi-lomba';
+
+        $(document).ready(function() {
+            // Dropdown tidak bisa di buka langsung sehingga perlu dipanggil
+            $('.dropdown-toggle').dropdown();
+
+            // DataTables
+            $(idDataTables).DataTable({
+                // Styling untuk pagination (Jangan diubah)
+                pagingType: "simple_numbers",
+                language: {
+                    lengthMenu: "Tampilkan _MENU_ entri",
+                    paginate: {
+                        first: "Pertama",
+                        previous: "Sebelum",
+                        next: "Lanjut",
+                        last: "Terakhir",
+                    },
+                    search: "Cari:",
+                    info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
+                },
+
+                // Bisa di Kustomisasi dengan data
+                serverSide: true,
+                autoWidth: true,
+                ajax: {
+                    url: "{{ route('verifikasi-lomba.list') }}",
+                    type: "POST",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                },
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        searchable: false,
+                    },
+                    {
+                        data: 'nama_lomba',
+                        name: 'nama_lomba',
+                    },
+                    {
+                        data: 'kategori',
+                        name: 'kategori',
+                    },
+                    {
+                        data: 'tingkat_lomba.nama_tingkat',
+                        name: 'tingkat_lomba.nama_tingkat',
+                    },
+                    {
+                        data: 'awal_registrasi_lomba',
+                        name: 'awal_registrasi_lomba',
+                    },
+                    {
+                        data: 'akhir_registrasi_lomba',
+                        name: 'akhir_registrasi_lomba',
+                    },
+                    {
+                        data: 'status_verifikasi',
+                        name: 'status_verifikasi',
+                    },
+                    {
+                        data: 'aksi',
+                        name: 'aksi',
+                        orderable: false,
+                        searchable: false,
+                    }
+                ],
+                scrollX: true,
+                layout: {
+                    topStart: null,
+                    topEnd: null,
+                    bottomStart: null,
+                    bottomEnd: null
+                },
+
+                // Styling untuk pagination (Jangan diubah)
+                drawCallback: function() {
+                    // Styling untuk pagination
+                    $(".dataTables_wrapper").css({
+                        margin: "0",
+                        padding: "0",
+                    });
+                    $(".dataTables_paginate .pagination").addClass("justify-content-end");
+                    $(".dataTables_paginate .paginate_button")
+                        .removeClass("paginate_button")
+                        .addClass("page-item");
+                    $(".dataTables_paginate .paginate_button a")
+                        .addClass("page-link")
+                        .css("border-radius", "5px");
+                    $(".dataTables_paginate .paginate_button.previous a").text("Sebelum");
+                    $(".dataTables_paginate .paginate_button.next a").text("Lanjut");
+                    $(".dataTables_paginate .paginate_button.first a").text("Pertama");
+                    $(".dataTables_paginate .paginate_button.last a").text("Terakhir");
+
+                    // Styling untuk input dan dropdown
+                    $(idDataTables + ' select').css({
+                        width: "auto",
+                        height: "auto",
+                        "border-radius": "5px",
+                        border: "1px solid #ced4da",
+                    });
+                    $(idDataTables + '_filter input').css({
+                        height: "auto",
+                        "border-radius": "5px",
+                        border: "1px solid #ced4da",
+                    });
+                    $(idDataTables + '_wrapper .table-bordered').css({
+                        "border-top-left-radius": "5px",
+                        "border-top-right-radius": "5px",
+                    });
+                    $(idDataTables + '_wrapper .dataTables_scrollBody table').css({
+                        "border-top-left-radius": "0px",
+                        "border-top-right-radius": "0px",
+                        "border-bottom-left-radius": "5px",
+                        "border-bottom-right-radius": "5px",
+                    });
+                }
+            });
+        });
+
+        // Fungsi untuk menampilkan modal
+        function modalAction(url) {
+            $.get(url)
+                .done(function(res) {
+                    $('#ajaxModalContent').html(res);
+                    $('#myModal').modal('show');
+                })
+                .fail(function() {
+                    Swal.fire('Gagal', 'Tidak dapat memuat data dari server.', 'error');
+                });
+        }
+
+        // Fungsi untuk reload dataTables
+        $(idDataTables).on('change', function() {
+            dataTables.ajax.reload();
+        })
+    </script>
+@endpush
+
---
+++ resources/views/admin/manajemen-lomba/verifikasi-lomba/show.blade.php
@@ -0,0 +1,111 @@
+<div class="modal-header bg-primary rounded">
+    <h5 class="modal-title text-white">Detail Lomba</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<div class="modal-body">
+    <table class="table table-bordered">
+        <tr>
+            <th style="width: 30%">Nama Lomba: </th>
+            <td class="text-start">{{ $lomba->nama_lomba }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Deskripsi: </th>
+            <td class="text-start">{{ $lomba->deskripsi_lomba }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Kategori: </th>
+            <td class="text-start">{{ $lomba->kategori->pluck('nama_kategori')->join(', ') ?: 'Tidak Diketahui' }}
+            </td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Tingkat: </th>
+            <td class="text-start">{{ $lomba->tingkat_lomba->nama_tingkat }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Penyelenggara: </th>
+            <td class="text-start">{{ $lomba->penyelenggara_lomba }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Awal Registrasi: </th>
+            <td class="text-start">{{ $lomba->awal_registrasi_lomba }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Akhir Registrasi: </th>
+            <td class="text-start">{{ $lomba->akhir_registrasi_lomba }}</td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Link Pendaftaran Lomba: </th>
+            <td class="text-start">
+                <a class="alert-primary" href="{{ $lomba->link_pendaftaran_lomba }}"
+                    target="_blank">{{ $lomba->link_pendaftaran_lomba }}</a>
+            </td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Status: </th>
+            <td class="text-start">
+                <span>
+                    @switch($lomba->status_verifikasi)
+                        @case('Menunggu')
+                            {{-- Terverifikasi --}}
+                            <span class="label label-warning">{{ $lomba->status_verifikasi }}</span>
+                        @break
+
+                        @case('Valid')
+                            {{-- Valid (diverifikasi admin) --}}
+                            <span class="label label-info">{{ $lomba->status_verifikasi }}</span>
+                        @break
+
+                        @default
+                            {{-- Ditolak --}}
+                            <span class="label label-danger">{{ $lomba->status_verifikasi }}</span>
+                    @endswitch
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <th style="width: 30%">Gambar Poster Lomba:</th>
+            <td class="text-start">
+                @if (!is_null($lomba->img_lomba) && file_exists(public_path('storage/img/lomba/' . $lomba->img_lomba)))
+                    <a href="{{ asset('storage/img/lomba/' . $lomba->img_lomba) }}" data-lightbox="lomba"
+                        data-title="Gambar Poster Lomba">
+                        <img src="{{ asset('storage/img/lomba/' . $lomba->img_lomba) }}" width="100"
+                            class="d-block mx-auto img-thumbnail" alt="Gambar Poster Lomba" style="cursor: zoom-in;" />
+                    </a>
+                @else
+                    <p class="text-center text-muted">Gambar tidak ada atau belum di upload</p>
+                @endif
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="modal-footer">
+    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">
+        <i class="fas fa-times"></i> Tutup
+    </button>
+</div>
+
+<style>
+    .lightbox .lb-data {
+        top: 0;
+        bottom: auto;
+        background: rgba(0, 0, 0, 0.7);
+    }
+
+    .lightbox .lb-data .lb-caption {
+        color: #fff;
+        padding: 10px;
+        font-size: 16px;
+        text-align: center;
+    }
+
+    .lightbox .lb-close {
+        top: 10px;
+        right: 10px;
+    }
+</style>
+
+{{-- Library Lightbox untuk membesarkan gambar --}}
+<link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/css/lightbox.min.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/js/lightbox.min.js"></script>
